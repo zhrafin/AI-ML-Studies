f11a7156483e68f80613e1832fc1c5db4c249454
Zayeed Hasan Rafin <139583706+zhrafin@users.noreply.github.com>
2026-03-09 01:36:30 +0600

    Add files via upload
    Basic Structure. Need to fix a lot of things here

---
+++ Langchain/apps/2.1_AI_Research_Assistant.py
@@ -0,0 +1,66 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+import streamlit as st
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langgraph.checkpoint.memory import InMemorySaver  
+
+st.subheader("Research Assistant - GPT OSS")
+
+llm = ChatGroq(model="openai/gpt-oss-20b", streaming=True)
+search = GoogleSerperAPIWrapper()
+checkpointer = InMemorySaver()
+
+if "memory" not in st.session_state:
+    st.session_state.memory = InMemorySaver()
+
+
+if "messeges" not in st.session_state:
+    st.session_state.messeges = []
+
+
+for messege in st.session_state.messeges:
+    role = messege["role"]
+    content = messege ["content"]
+    st.chat_message(role).markdown(content)
+
+
+
+agent = create_agent(
+    model=llm, 
+    tools=[search.run],
+    checkpointer=checkpointer,
+    system_prompt="You are Expert Research Assistant who searches in the web For valid source and answers from the sources with proof (eg. link of the websites, name of the website) attached to the answer."
+)
+
+query = st.chat_input("Ask Your Research Assistant... ")
+
+if query:
+    st.chat_message("user").markdown(query)
+    st.session_state.messeges.append({"role": "user", "content": query})
+
+    res =  agent.stream(
+        {"messages":[{"role": "user", "content": query}]},
+        {"configurable": {"thread_id": "rafinAI"}},
+        stream_mode="messages"
+    )
+
+    res_container = st.chat_message("ai")
+
+    with res_container:
+        space = st.space() 
+        
+        response = ""
+
+        for chunk in res:
+            response = response + chunk[0].content
+            space.write(response)
+
+        st.session_state.messeges.append({"role": "ai", "content": response})
+
+
+
+
+
