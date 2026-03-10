8d4570d96a076aaa63f1be5585db0358461951fb
Zayeed Hasan Rafin <139583706+zhrafin@users.noreply.github.com>
2026-03-10 13:50:22 +0600

    Update 2.1_AI_Research_Assistant.py
    a lot of changes

--- Langchain/apps/2.1_AI_Research_Assistant.py
+++ Langchain/apps/2.1_AI_Research_Assistant.py
@@ -7,6 +7,7 @@ import streamlit as st
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langgraph.checkpoint.memory import InMemorySaver  
 
+st.set_page_config(page_title="Research Assistant", page_icon="🤖",)
 st.subheader("Research Assistant - GPT OSS")
 
 llm = ChatGroq(model="openai/gpt-oss-20b", streaming=True)
@@ -16,15 +17,95 @@ checkpointer = InMemorySaver()
 if "memory" not in st.session_state:
     st.session_state.memory = InMemorySaver()
 
+# Notun chat toiri
+if "sessions" not in st.session_state:
+    st.session_state.sessions = {
+        "Chat 1":[]
+    }
 
-if "messeges" not in st.session_state:
-    st.session_state.messeges = []
+# ekhon kon chat khola ase janar jonno
+if "current_chat" not in st.session_state:
+    st.session_state.current_chat = "Chat 1"
 
 
-for messege in st.session_state.messeges:
-    role = messege["role"]
-    content = messege["content"]
+if "chat_counter" not in st.session_state:
+    st.session_state.chat_counter = 1
+
+# current_chat er shob messages string akare nicchi
+# current_chat direct nile just CHat 1 ashbe. texts ashbe  current method e
+
+#st.session_state.sessions e thake
+"""st.session_state.sessions = {
+    "Chat 1": [
+        {"role": "user", "content": "Hello"},
+        {"role": "assistant", "content": "Hi"}
+    ],
+    "Chat 2": [
+        {"role": "user", "content": "Explain AI"}
+    ]
+}
+"""
+current_messages = st.session_state.sessions[st.session_state.current_chat]
+
+# Existing message print korte thakbo
+for message in current_messages:
+    role = message["role"]
+    content = message["content"]
     st.chat_message(role).markdown(content)
+
+
+# Sidebar Start
+with st.sidebar:
+    st.header("Settings")
+
+    model_name = st.selectbox(
+        "Select model",
+        [
+            "openai/gpt-oss-20b",
+            "openai/gpt-oss-120b",
+            "llama-3.1-8b-instant",
+        ], 
+        index=0
+    )
+
+    enable_web_search = st.checkbox(
+        "Enable Web Search",
+        value=True
+    )
+
+    st.divider()
+    st.subheader("Chats")
+
+    # sessions er value gula key wise jabe jehetu ekta list
+    chat_names = list(st.session_state.sessions.keys())
+    
+    selected_chat=st.selectbox(
+            "Select Chat",
+            chat_names,
+            # active chat er position ber kore box e show kore
+            index=chat_names.index(st.session_state.current_chat)
+        )
+    
+    # new current chat e jaoa
+    st.session_state.current_chat = selected_chat
+ 
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("New Chat"):
+            st.session_state.chat_counter +=1
+            new_chat_name = f"Chat {st.session_state.chat_counter}"
+            st.session_state.sessions[new_chat_name] = []
+            st.session_state.current_chat = new_chat_name
+            #proti action er por rerun
+            st.rerun()
+    with col2:
+        if st.button("Delete Chat"):
+            if (st.session_state.chat_counter > 1):
+                del st.session_state.sessions[st.session_state.current_chat]
+                st.session_state.current_chat = list(st.session_state.sessions.keys())[0]
+                st.rerun()
+
 
 
 
@@ -66,3 +147,4 @@ if query:
 
     st.chat_message("ai").markdown(res["messages"][-1].content)
     st.session_state.messeges.append({"role": "ai", "content": res["messages"][-1].content})
+
